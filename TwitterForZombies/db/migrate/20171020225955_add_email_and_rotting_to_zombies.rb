2c300580982dab64adae303fbd8cdfa57f45990d
639dev <gharood_3@hotmail.com>
2017-11-06 01:09:34 +0300

    Add genres through bookgenres

--- TwitterForZombies/db/migrate/20171020225955_add_email_and_rotting_to_zombies.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailAndRottingToZombies < ActiveRecord::Migration[5.0]
-  def change
-    add_column :zombies, :email, :String
-    add_column :zombies, :rotting, :boolean,default: false
-  end
-end
